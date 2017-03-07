f6f1a91e9454dcf5f9f0b488ee321b88d3eba55d
Jing Wang <yuzhounh@163.com>
2017-03-07 16:32:52 +0800

    Delete m1_main.m

--- m1_main.m
+++
@@ -1,9 +0,0 @@
-% The main script.
-% 2016-4-11 22:39:59
-
-clear,clc;
-
-m1_download; % download the toolboxes and the fMRI data
-m1_load; % check the data size
-m1_prepare; % some prior steps before classification
-m1_classify; 
